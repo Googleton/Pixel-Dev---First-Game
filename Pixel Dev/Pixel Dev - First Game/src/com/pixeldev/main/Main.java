f940b5958babd2962b47d402625ac542fc1e2e2a
XDki113r <guillaumejean46@gmail.com>
2013-04-13 09:20:53 -0400

    lulz changé

--- Pixel Dev/Pixel Dev - First Game/src/com/pixeldev/main/Main.java
+++ Pixel Dev/Pixel Dev - First Game/src/com/pixeldev/main/Main.java
@@ -2,7 +2,7 @@ package com.pixeldev.main;
 
 public class Main {
 
-	public static int lulz = 15;
+	public static int lulz = 188581;
 	
 	public static void main(String[] args) {
 		System.out.println("Hello World !" + lulz);
