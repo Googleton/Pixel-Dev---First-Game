08e594eb2a9f2c2f66ea59694212a3c93c01b3f5
Gugu42 <gugo42800@gmail.com>
2013-04-16 13:30:03 +0200

    (+) quitButton.png (+) playButton.png

--- Pixel Dev/Pixel Dev - First Game/src/com/pixeldev/states/MenuState.java
+++ Pixel Dev/Pixel Dev - First Game/src/com/pixeldev/states/MenuState.java
@@ -13,36 +13,36 @@ public class MenuState extends BasicGameState {
 	public Image playButton;
 	public Image quitButton;
 	public int playBx, playBy;
+	public int quitBx, quitBy;
 	
 	@Override
 	public void init(GameContainer arg0, StateBasedGame arg1)
 			throws SlickException {
-		playBx = 150;
+		playBx = 540;
 		playBy = 150;
+		quitBx = 515;
+		quitBy = 200;
 		playButton = new Image("/res/guis/playButton.png");
+		quitButton = new Image("/res/guis/quitButton.png");
 	}
 
 	@Override
 	public void render(GameContainer arg0, StateBasedGame arg1, Graphics arg2)
 			throws SlickException {
-		arg2.drawString("Clickez pour démarrer, Echap pour quitter",
-				(1280 / 2 - 80), (720 / 2));
 		playButton.draw(playBx, playBy);
-
+		quitButton.draw(quitBx, quitBy);
 	}
 
 	@Override
 	public void update(GameContainer gc, StateBasedGame sbg, int delta)
 			throws SlickException {
-		if (gc.getInput().isMousePressed(Input.MOUSE_LEFT_BUTTON)) {
-			sbg.enterState(1);
-		} else if (gc.getInput().isKeyPressed(Input.KEY_ESCAPE)) {
-			gc.exit();
-		}
 		
 		if(gc.getInput().getMouseX() > playBx && gc.getInput().getMouseY() > playBy && gc.getInput().getMouseX() < playBx + playButton.getWidth()
-				&& gc.getInput().getMouseY() < playBy + playButton.getHeight()){
+				&& gc.getInput().getMouseY() < playBy + playButton.getHeight() && gc.getInput().isMousePressed(Input.MOUSE_LEFT_BUTTON)){
 			sbg.enterState(1);
+		} else if (gc.getInput().getMouseX() > quitBx && gc.getInput().getMouseY() > quitBy && gc.getInput().getMouseX() < quitBx + quitButton.getWidth()
+				&& gc.getInput().getMouseY() < quitBy + quitButton.getHeight() && gc.getInput().isMousePressed(Input.MOUSE_LEFT_BUTTON)) {
+			gc.exit();
 		}
 
 	}
